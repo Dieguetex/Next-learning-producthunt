616607a00e747bc2d9d9cf13b85cd2f34b98a38d
PEREZ MANSILLA Diego <diego.perez2@soprasteria.com>
2020-08-03 13:41:32 +0200

    Form changes + firebase config

--- pages/crear-cuenta.js
+++ pages/crear-cuenta.js
@@ -1,56 +1,102 @@
 import React, { Fragment } from "react";
 import { css } from "@emotion/core";
 import Layout from "../components/layout/Layout";
-import { Formulario, Campo, InputSubmit } from "../components/ui/Formulario";
+import {
+  Formulario,
+  Campo,
+  InputSubmit,
+  Error,
+} from "../components/ui/Formulario";
 
-const CrearCuenta = () => (
-  <div>
-    <Layout>
-      <Fragment>
-        <h1
-          css={css`
-            text-align: center;
-            margin-top: 5rem;
-          `}
-        >
-          Crear Cuenta
-        </h1>
-        <Formulario>
-          <Campo>
-            <label htmlFor="nombre">Nombre</label>
-            <input
-              type="text"
-              id="nombre"
-              placeholder="Tu Nombre"
-              name="nombre"
-            />
-          </Campo>
+// Validaciones
+import useValidacion from "../hooks/useValidacion";
+import validarCrearCuenta from "../validacion/validarCrearCuenta";
 
-          <Campo>
-            <label htmlFor="nombre">Email</label>
-            <input
-              type="email"
-              id="email"
-              placeholder="Tu Email"
-              name="email"
-            />
-          </Campo>
+const STATE_INICIAL = {
+  nombre: "",
+  email: "",
+  password: "",
+};
 
-          <Campo>
-            <label htmlFor="nombre">Password</label>
-            <input
-              type="password"
-              id="password"
-              placeholder="Tu Password"
-              name="password"
-            />
-          </Campo>
+const CrearCuenta = () => {
+  const {
+    valores,
+    errores,
+    handleChange,
+    handleSubmit,
+    handleBlur,
+  } = useValidacion(STATE_INICIAL, validarCrearCuenta, crearCuenta);
 
-          <InputSubmit type="submit" value="Crear Cuenta" />
-        </Formulario>
-      </Fragment>
-    </Layout>
-  </div>
-);
+  const { nombre, email, password } = valores;
+
+  function crearCuenta() {
+    console.log("Creando cuenta...");
+  }
+
+  return (
+    <div>
+      <Layout>
+        <Fragment>
+          <h1
+            css={css`
+              text-align: center;
+              margin-top: 5rem;
+            `}
+          >
+            Crear Cuenta
+          </h1>
+          <Formulario onSubmit={handleSubmit} noValidate>
+            <Campo>
+              <label htmlFor="nombre">Nombre</label>
+              <input
+                type="text"
+                id="nombre"
+                placeholder="Tu Nombre"
+                name="nombre"
+                value={nombre}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+            </Campo>
+
+            {errores.nombre && <Error>{errores.nombre}</Error>}
+
+            <Campo>
+              <label htmlFor="nombre">Email</label>
+              <input
+                type="email"
+                id="email"
+                placeholder="Tu Email"
+                name="email"
+                value={email}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+            </Campo>
+
+            {errores.email && <Error>{errores.email}</Error>}
+
+            <Campo>
+              <label htmlFor="nombre">Password</label>
+              <input
+                type="password"
+                id="password"
+                placeholder="Tu Password"
+                name="password"
+                value={password}
+                onChange={handleChange}
+                onBlur={handleBlur}
+              />
+            </Campo>
+
+            {errores.password && <Error>{errores.password}</Error>}
+
+            <InputSubmit type="submit" value="Crear Cuenta" />
+          </Formulario>
+        </Fragment>
+      </Layout>
+    </div>
+  );
+};
 
 export default CrearCuenta;
