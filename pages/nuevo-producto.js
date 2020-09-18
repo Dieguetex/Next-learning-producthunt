1f8029be93f7f3072d3fc5a28e59278cbddbc19e
PEREZ MANSILLA Diego <diego.perez2@soprasteria.com>
2020-09-18 15:12:23 +0200

    inicio finished

--- pages/nuevo-producto.js
+++ pages/nuevo-producto.js
@@ -12,6 +12,8 @@ import {
 
 import { FirebaseContext } from "../firebase";
 
+import Error404 from "../components/layout/404";
+
 // Validaciones
 import useValidacion from "../hooks/useValidacion";
 import validarCrearProducto from "../validacion/validarCrearProducto";
@@ -49,6 +51,8 @@ const NuevoProducto = () => {
   // Context con las operaciones crud de firebase
   const { usuario, firebase } = useContext(FirebaseContext);
 
+  console.log("usuario", usuario);
+
   async function crearProducto() {
     // Si el usuario no está autenticado redirigir al login
     if (!usuario) {
@@ -65,6 +69,11 @@ const NuevoProducto = () => {
       votos: 0,
       comentarios: [],
       creado: Date.now(),
+      creador: {
+        id: usuario.uid,
+        nombre: usuario.displayName,
+      },
+      haVotado: [],
     };
 
     // Insertar producto en base de datos
@@ -103,102 +112,106 @@ const NuevoProducto = () => {
   return (
     <div>
       <Layout>
-        <Fragment>
-          <h1
-            css={css`
-              text-align: center;
-              margin-top: 5rem;
-            `}
-          >
-            Nuevo Producto
-          </h1>
-          <Formulario onSubmit={handleSubmit} noValidate>
-            <fieldset>
-              <legend>Información General</legend>
+        {!usuario ? (
+          <Error404 />
+        ) : (
+          <Fragment>
+            <h1
+              css={css`
+                text-align: center;
+                margin-top: 5rem;
+              `}
+            >
+              Nuevo Producto
+            </h1>
+            <Formulario onSubmit={handleSubmit} noValidate>
+              <fieldset>
+                <legend>Información General</legend>
 
-              <Campo>
-                <label htmlFor="nombre">Nombre</label>
-                <input
-                  type="text"
-                  id="nombre"
-                  placeholder="Tu Nombre"
-                  name="nombre"
-                  value={nombre}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                />
-              </Campo>
+                <Campo>
+                  <label htmlFor="nombre">Nombre</label>
+                  <input
+                    type="text"
+                    id="nombre"
+                    placeholder="Nombre del Producto"
+                    name="nombre"
+                    value={nombre}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                </Campo>
 
-              {errores.nombre && <Error>{errores.nombre}</Error>}
+                {errores.nombre && <Error>{errores.nombre}</Error>}
 
-              <Campo>
-                <label htmlFor="empresa">Empresa</label>
-                <input
-                  type="text"
-                  id="empresa"
-                  placeholder="Nombre de empresa o compañía"
-                  name="empresa"
-                  value={empresa}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                />
-              </Campo>
+                <Campo>
+                  <label htmlFor="empresa">Empresa</label>
+                  <input
+                    type="text"
+                    id="empresa"
+                    placeholder="Nombre de empresa o compañía"
+                    name="empresa"
+                    value={empresa}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                </Campo>
 
-              {errores.empresa && <Error>{errores.empresa}</Error>}
+                {errores.empresa && <Error>{errores.empresa}</Error>}
 
-              <Campo>
-                <label htmlFor="imagen">Imagen</label>
-                <FileUploader
-                  accept="image/*"
-                  id="imagen"
-                  name="imagen"
-                  randomizeFilename
-                  storageRef={firebase.storage.ref("productos")}
-                  onUploadStart={handleUploadStart}
-                  onUploadError={handleUploadError}
-                  onUploadSuccess={handleUploadSuccess}
-                  onProgress={handleProgress}
-                />
-              </Campo>
+                <Campo>
+                  <label htmlFor="imagen">Imagen</label>
+                  <FileUploader
+                    accept="image/*"
+                    id="imagen"
+                    name="imagen"
+                    randomizeFilename
+                    storageRef={firebase.storage.ref("productos")}
+                    onUploadStart={handleUploadStart}
+                    onUploadError={handleUploadError}
+                    onUploadSuccess={handleUploadSuccess}
+                    onProgress={handleProgress}
+                  />
+                </Campo>
 
-              <Campo>
-                <label htmlFor="url">URL</label>
-                <input
-                  type="url"
-                  id="url"
-                  name="url"
-                  placeholder="URL de tu producto"
-                  value={url}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                />
-              </Campo>
+                <Campo>
+                  <label htmlFor="url">URL</label>
+                  <input
+                    type="url"
+                    id="url"
+                    name="url"
+                    placeholder="URL de tu producto"
+                    value={url}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                </Campo>
 
-              {errores.url && <Error>{errores.url}</Error>}
-            </fieldset>
+                {errores.url && <Error>{errores.url}</Error>}
+              </fieldset>
 
-            <fieldset>
-              <legend>Sobre tu Producto</legend>
+              <fieldset>
+                <legend>Sobre tu Producto</legend>
 
-              <Campo>
-                <label htmlFor="descripcion">Descripción</label>
-                <textarea
-                  id="descripcion"
-                  name="descripcion"
-                  value={descripcion}
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                />
-              </Campo>
+                <Campo>
+                  <label htmlFor="descripcion">Descripción</label>
+                  <textarea
+                    id="descripcion"
+                    name="descripcion"
+                    value={descripcion}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                  />
+                </Campo>
 
-              {errores.descripcion && <Error>{errores.descripcion}</Error>}
-            </fieldset>
+                {errores.descripcion && <Error>{errores.descripcion}</Error>}
+              </fieldset>
 
-            {error && <Error>{error}</Error>}
+              {error && <Error>{error}</Error>}
 
-            <InputSubmit type="submit" value="Crear Producto" />
-          </Formulario>
-        </Fragment>
+              <InputSubmit type="submit" value="Crear Producto" />
+            </Formulario>
+          </Fragment>
+        )}
       </Layout>
     </div>
   );
--- pages/productos/[id].js
+++ pages/productos/[id].js
@@ -9,6 +9,8 @@ import styled from "@emotion/styled";
 
 import formatDistanceToNow from "date-fns/formatDistanceToNow";
 import { es } from "date-fns/locale";
+import { Campo, InputSubmit } from "../../components/ui/Formulario";
+import Boton from "../../components/ui/Boton";
 
 const ContenedorProducto = styled.div`
   @media (min-width: 768px) {
@@ -18,10 +20,23 @@ const ContenedorProducto = styled.div`
   }
 `;
 
+const CreadorProducto = styled.p`
+  padding: 0.5rem 2rem;
+  margin: 0;
+  background-color: #da552f;
+  color: #fff;
+  text-transform: uppercase;
+  font-weight: bold;
+  display: inline-block;
+  text-align: center;
+`;
+
 const Producto = () => {
   // State del componente
   const [producto, guardarProducto] = useState({});
   const [error, guardarError] = useState(false);
+  const [comentario, guardarComentario] = useState({});
+  const [consultarDB, guardarconsultarDB] = useState(true);
 
   // Routing para obtener el id actual
   const router = useRouter();
@@ -30,26 +45,28 @@ const Producto = () => {
   } = router;
 
   // Context de firebase
-  const { firebase } = useContext(FirebaseContext);
+  const { firebase, usuario } = useContext(FirebaseContext);
 
   useEffect(() => {
-    if (id) {
+    if (id && consultarDB) {
       const obtenerProducto = async () => {
         const productoQuery = await firebase.db.collection("productos").doc(id);
         const producto = await productoQuery.get();
-        guardarProducto(producto.data());
 
         if (producto.exists) {
           guardarProducto(producto.data());
         } else {
           guardarError(true);
         }
+        guardarconsultarDB(false);
       };
       obtenerProducto();
     }
   }, [id]);
 
-  if (Object.keys(producto).length === 0) return "Cargando...";
+  console.log(producto);
+
+  if (Object.keys(producto).length === 0 && !error) return "Cargando...";
 
   const {
     /* id, */
@@ -61,38 +78,231 @@ const Producto = () => {
     url,
     urlimagen,
     votos,
+    creador,
+    haVotado,
   } = producto;
+
+  // Administrar y validar los votos
+  const votarProducto = () => {
+    if (!usuario) {
+      return router.push("/login");
+    }
+
+    // Obtener y sumar nuevo voto
+    const nuevoTotal = votos + 1;
+
+    // Verificar si el usuario actual ha votado
+    if (haVotado.includes(usuario.uid)) return;
+
+    // Guardar ID del usuario que ha votado
+    const nuevoHaVotado = [...haVotado, usuario.uid];
+
+    // Actualizar BD
+    firebase.db
+      .collection("productos")
+      .doc(id)
+      .update({ votos: nuevoTotal, haVotado: nuevoHaVotado });
+
+    // Actualizar el state
+    guardarProducto({
+      ...producto,
+      votos: nuevoTotal,
+    });
+
+    guardarconsultarDB(true); // Hay un voto, por lo tanto consultar a la BD
+  };
+
+  // Funciones para crear comentario
+  const comentarioChange = (e) => {
+    guardarComentario({
+      ...comentario,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  // Identifica si el comentario es del creador del producto
+  const esCreador = (id) => {
+    if (creador.id === id) {
+      return true;
+    }
+  };
+
+  const agregarComentario = (e) => {
+    e.preventDefault();
+
+    document.getElementById("form").reset();
+
+    if (!usuario) {
+      return router.push("/login");
+    }
+
+    // Información extra al comentario
+    comentario.usuarioId = usuario.uid;
+    comentario.usuarioNombre = usuario.displayName;
+
+    // Hacer compia de comentarios y aañadirlos al array
+    const nuevosComentarios = [...comentarios, comentario];
+
+    // Actualizar BD
+    firebase.db.collection("productos").doc(id).update({
+      comentarios: nuevosComentarios,
+    });
+
+    // Actualizar el state
+    guardarProducto({
+      ...producto,
+      comentarios: nuevosComentarios,
+    });
+
+    guardarconsultarDB(true); // Hay un voto, por lo tanto consultar a la BD
+  };
+
+  // Función que revisa que el creador del producto sea el mismo que está logeado
+  const puedeBorrar = () => {
+    if (!usuario) return false;
+
+    if (creador.id === usuario.uid) return true;
+  };
+
+  // Elimina un producto de la BD
+
+  const eliminarProducto = async () => {
+    if (!usuario) return router.push("/login");
+    if (creador.id !== usuario.uid) return router.push("/");
+
+    try {
+      await firebase.db.collection("productos").doc(id).delete();
+      router.push("/");
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return (
     <Layout>
       <>
-        {error && <Error404 />}
+        {error ? (
+          <Error404 />
+        ) : (
+          <div className="contenedor">
+            <h1
+              css={css`
+                text-align: center;
+                margin-top: 5rem;
+              `}
+            >
+              {nombre}
+            </h1>
 
-        <div className="contenedor">
-          <h1
-            css={css`
-              text-align: center;
-              margin-top: 5rem;
-            `}
-          >
-            {nombre}
-          </h1>
+            <ContenedorProducto>
+              <div>
+                {" "}
+                <p>
+                  Publicado hace:{" "}
+                  {formatDistanceToNow(new Date(creado), { locale: es })}
+                </p>
+                <p>
+                  Por: {creador.nombre} de {empresa}
+                </p>
+                <img src={urlimagen} />
+                <p>{descripcion}</p>
+                {usuario && (
+                  <>
+                    <h2>Agrega tu comentario</h2>
 
-          <ContenedorProducto>
-            <div>
-              {" "}
-              <p>
-                Publicado hace:{" "}
-                {formatDistanceToNow(new Date(creado), { locale: es })}
-              </p>
-              <img src={urlimagen} />
-              <p>{descripcion}</p>
-              <h2>Agrega tu comentario</h2>
-            </div>
+                    <form id="form" onSubmit={agregarComentario}>
+                      <Campo>
+                        <input
+                          type="text"
+                          name="mensaje"
+                          onChange={comentarioChange}
+                        />
+                      </Campo>
 
-            <aside>2</aside>
-          </ContenedorProducto>
-        </div>
+                      <InputSubmit type="submit" value="Agregar Comentario" />
+                    </form>
+                  </>
+                )}
+                <h2
+                  css={css`
+                    margin: 2rem 0;
+                  `}
+                >
+                  Comentarios
+                </h2>
+                {comentarios.length === 0 ? (
+                  "Aún no hay comentarios"
+                ) : (
+                  <ul>
+                    {comentarios.map((comentario, i) => (
+                      <li
+                        key={`${comentario.usuarioId}-${i}`}
+                        css={css`
+                          border: 1px solid #e1e1e1;
+                          padding: 2rem;
+                        `}
+                      >
+                        <p>{comentario.mensaje}</p>
+                        <p
+                          css={css`
+                            color: #9a9a9a;
+                          `}
+                        >
+                          Escrito por:
+                          <span
+                            css={css`
+                              font-weight: bold;
+                            `}
+                          >
+                            {" "}
+                            {comentario.usuarioNombre}
+                          </span>
+                        </p>
+
+                        {esCreador(comentario.usuarioId) && (
+                          <CreadorProducto>Creador</CreadorProducto>
+                        )}
+                      </li>
+                    ))}
+                  </ul>
+                )}
+              </div>
+
+              <aside>
+                <Boton target="_blank" bgColor="true" href={url}>
+                  Visitar URL
+                </Boton>
+
+                <div
+                  css={css`
+                    margin-top: 5rem;
+                  `}
+                >
+                  <p
+                    css={css`
+                      text-align: center;
+                    `}
+                  >
+                    {votos} Votos
+                  </p>
+                </div>
+
+                {usuario && <Boton onClick={votarProducto}>Votar</Boton>}
+              </aside>
+            </ContenedorProducto>
+
+            {puedeBorrar() && (
+              <Boton
+                onClick={eliminarProducto}
+                css={css`
+                  margin-top: 2rem;
+                `}
+              >
+                Eliminar Producto
+              </Boton>
+            )}
+          </div>
+        )}
       </>
     </Layout>
   );
